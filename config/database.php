dc7eae8318f93b1a6ca38b47869c1dbe2656adbc
SANDI <sandi@SANDIs-MacBook-Pro.local>
2026-04-02 00:29:49 +0700

    update

--- config/database.php
+++ config/database.php
@@ -57,7 +57,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (class_exists(PDO\Mysql::class) ? PDO\Mysql::ATTR_SSL_CA : \Pdo\Mysql::ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [
@@ -75,7 +77,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' =>  [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (class_exists(PDO\Mysql::class) ? PDO\Mysql::ATTR_SSL_CA : \Pdo\Mysql::ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
