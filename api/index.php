be4722083316ed78ac85bfbf50e82865cfaadfa9
unknown <User@DESKTOP-DO052JA.tap.corp>
2026-04-02 15:40:21 +0700

    fix the bug

--- api/index.php
+++ api/index.php
@@ -1,22 +1,5 @@
 <?php
 
-// Buat semua direktori yang dibutuhkan Laravel di /tmp (writable di Vercel)
-$dirs = [
-    '/tmp/storage/logs',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/sessions',
-    '/tmp/bootstrap/cache',
-];
-
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
-
-// Override path Laravel ke /tmp
-$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
 
 // Force HTTPS
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
