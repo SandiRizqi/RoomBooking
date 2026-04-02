d2bb196aabc062d60c07a8bc0e4904a2c6830adc
unknown <User@DESKTOP-DO052JA.tap.corp>
2026-04-02 15:46:36 +0700

    fix the bug

--- api/index.php
+++ api/index.php
@@ -1,9 +1,25 @@
 <?php
 
-
 // Force HTTPS
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
+}
+
+// Buat direktori /tmp yang dibutuhkan Laravel agar writable di Vercel
+$directories = [
+    '/tmp/bootstrap/cache',
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/testing',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
 }
 
 require __DIR__ . '/../public/index.php';
