a142dc72cd1463d944f2af2d3e083dbcabf26471
SANDI <sandi@SANDIs-MacBook-Pro.local>
2026-04-10 14:15:16 +0700

    add calender event

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // API routes
+Route::get('/availability-events', [PageController::class, 'calendarEvents'])->name('api.availability');
